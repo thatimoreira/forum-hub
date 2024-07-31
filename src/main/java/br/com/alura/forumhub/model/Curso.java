5bb6883eb90bb204b855f96130e9d30964df7535
thatianamoreira <thatianamoreira@id.uff.br>
2024-07-31 07:48:05 -0300

    feat: Adiciona modelagem do banco de dados e script de criação de tabelas (Github Projects: #3)
    - Criação das entidades Curso, Usuario, StatusTopico e Topico
    - Adiciona script de migração para criação das tabelas no banco de dados
    - Geração e inclusão do diagrama ER do banco de dados

---
+++ src/main/java/br/com/alura/forumhub/model/Curso.java
@@ -0,0 +1,39 @@
+package br.com.alura.forumhub.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
---
+++ src/main/java/br/com/alura/forumhub/model/Topico.java
@@ -0,0 +1,91 @@
+package br.com.alura.forumhub.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String mensagem;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_topico")
+    private StatusTopico statusTopico = StatusTopico.NAO_RESPONDIDO;
+
+    @ManyToOne
+    private Usuario autor;
+
+    @ManyToOne
+    private Curso curso;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public StatusTopico getStatusTopico() {
+        return statusTopico;
+    }
+
+    public void setStatusTopico(StatusTopico statusTopico) {
+        this.statusTopico = statusTopico;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+}
